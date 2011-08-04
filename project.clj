1cbdada90248c81f6d028c9c2b6ecc4758d22eeb
seungjin <seungjin@seungjin.net>
2011-08-03 21:24:27 -0700

    add clojurecontrib:

--- project.clj
+++ project.clj
@@ -2,9 +2,11 @@
   :dependencies
   [
    [org.clojure/clojure "1.2.1"]
+   [org.clojure/clojure-contrib "1.2.0"]
    [ring/ring-jetty-adapter "0.3.9"]
    [compojure "0.6.4"]
   ]
+  :repositories {"clojure-releases" "http://build.clojure.org/releases"}
 )
 
 
